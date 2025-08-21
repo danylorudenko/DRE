f17f0a7ddd43353124a743b6721bc8e1e83d7345
Danylo Rudenko <rudenko.dany@gmail.com>
2025-08-21 21:03:36 +0200

    Compiled fft_inv_perm.comp

--- shaders/fft_water_uniform.h
+++ shaders/fft_water_uniform.h
@@ -1,15 +1,16 @@
+struct FFTWaterUniform
 {
-        float4 N_winddir_noiseID;
-        float4 windSpeed_waterSpeed_waterSizeMeters;
-        float4 windDirectionality;
-} data;
+    float4 N_winddir_noiseID;
+    float4 windSpeed_waterSpeed_waterSizeMeters;
+    float4 windDirectionality;
+};
 
-float GetFFTSize() { return data.N_winddir_noiseID.x; }
-float2 GetWindDir() { return data.N_winddir_noiseID.yz; }
-float GetNoiseTextureID() { return data.N_winddir_noiseID.w; }
-float GetWindSpeed() { return data.windSpeed_waterSpeed_waterSizeMeters.x; }
-float GetWaterSimSpeed() { return data.windSpeed_waterSpeed_waterSizeMeters.y; }
-float GetWaterPatchSize() { return data.windSpeed_waterSpeed_waterSizeMeters.z; }
-float GetWaterAmplitude() { return data.windSpeed_waterSpeed_waterSizeMeters.w; }
-float GetWaterLargestWave() { return pow(GetWindSpeed(), 2.0f) / 9.8f; }
-float GetWindDirFactor() { return data.windDirectionality.x; }
+float GetFFTSize(FFTWaterUniform data) { return data.N_winddir_noiseID.x; }
+float2 GetWindDir(FFTWaterUniform data) { return data.N_winddir_noiseID.yz; }
+float GetNoiseTextureID(FFTWaterUniform data) { return data.N_winddir_noiseID.w; }
+float GetWindSpeed(FFTWaterUniform data) { return data.windSpeed_waterSpeed_waterSizeMeters.x; }
+float GetWaterSimSpeed(FFTWaterUniform data) { return data.windSpeed_waterSpeed_waterSizeMeters.y; }
+float GetWaterPatchSize(FFTWaterUniform data) { return data.windSpeed_waterSpeed_waterSizeMeters.z; }
+float GetWaterAmplitude(FFTWaterUniform data) { return data.windSpeed_waterSpeed_waterSizeMeters.w; }
+float GetWaterLargestWave(FFTWaterUniform data) { return pow(GetWindSpeed(data), 2.0f) / 9.8f; }
+float GetWindDirFactor(FFTWaterUniform data) { return data.windDirectionality.x; }
