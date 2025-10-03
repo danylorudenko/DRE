6fbf77669f58c257acc746017f0136ade7343628
Danylo Rudenko <rudenko.dany@gmail.com>
2025-10-03 22:48:52 +0200

    Convenience functions to read material properties in shaders (S_MATERIAL_PROPERTIES)

--- shaders/common/materials.h
+++ shaders/common/materials.h
@@ -12,13 +12,7 @@ enum MaterialFlags : DRE::U32
     MATERIAL_FLAG_MATERIAL_TEXTURES_DEFAULT      = 1u << 3,
     MATERIAL_FLAG_MATERIAL_TEXTURES_GLTF_SPHERES = 1u << 4
 };
-#else
-#define MATERIAL_FLAG_NORMAL_TEXTURE                 (1 << 0)
-#define MATERIAL_FLAG_NORMAL_TEXTURE_INVERT_Y        (1 << 1)
-#define MATERIAL_FLAG_NORMAL_TBN                     (1 << 2)
-#define MATERIAL_FLAG_MATERIAL_TEXTURES_DEFAULT      (1 << 3)
-#define MATERIAL_FLAG_MATERIAL_TEXTURES_GLTF_SPHERES (1 << 4)
-#endif
+#endif // __cplusplus
 
 struct S_MATERIAL
 {
@@ -27,7 +21,63 @@ struct S_MATERIAL
     uint4 flags;
 };
 
+
 #ifndef __cplusplus
+#include "common/shaders_common.h"
+
+#define MATERIAL_FLAG_NORMAL_TEXTURE                 (1 << 0)
+#define MATERIAL_FLAG_NORMAL_TEXTURE_INVERT_Y        (1 << 1)
+#define MATERIAL_FLAG_NORMAL_TBN                     (1 << 2)
+#define MATERIAL_FLAG_MATERIAL_TEXTURES_DEFAULT      (1 << 3)
+#define MATERIAL_FLAG_MATERIAL_TEXTURES_GLTF_SPHERES (1 << 4)
+
+struct S_MATERIAL_PROPERTIES
+{
+    float3 diffuse = float3(0,0,0);
+    float3 normal = float3(0,0,1);
+    float metalness = 0;
+    float roughness = 0;
+};
+
+S_MATERIAL_PROPERTIES ReadMaterialProeprties(S_MATERIAL* material, float2 UV, float3x3 TBN)
+{
+    S_MATERIAL_PROPERTIES result = {};
+
+    uint materialFlags = GetMaterialFlags(material);
+
+    result.diffuse    = SampleGlobalTextureAnisotropic(GetDiffuseTextureID(material), UV).rgb;
+    result.normal     = SampleGlobalTextureAnisotropic(GetNormalTextureID(material), UV).rgb;
+
+    if ((materialFlags & MATERIAL_FLAG_MATERIAL_TEXTURES_DEFAULT) != 0)
+    {
+        result.metalness = SampleGlobalTextureAnisotropic(GetMetalnessTextureID(material), UV).r;
+        result.roughness = SampleGlobalTextureAnisotropic(GetRoughnessTextureID(material), UV).r;
+    }
+    if ((materialFlags & MATERIAL_FLAG_MATERIAL_TEXTURES_GLTF_SPHERES) != 0)
+    {
+        float2 metalness_roughness = SampleGlobalTextureAnisotropic(GetMetalnessTextureID(material), UV).bg;
+        result.metalness = metalness_roughness.x;
+        result.roughness = metalness_roughness.y;
+    }
+
+    if ((materialFlags & MATERIAL_FLAG_NORMAL_TEXTURE) != 0)
+    {
+        result.normal = normalize(mul(TBN, (result.normal * 2.0 - 1.0)));
+    }
+
+    if ((materialFlags & MATERIAL_FLAG_NORMAL_TEXTURE_INVERT_Y) != 0)
+    {
+        result.normal = float3(result.normal.r, 1 - result.normal.g, result.normal.b);
+        result.normal = normalize(mul(TBN, (result.normal * 2.0 - 1.0)));
+    }
+
+    if ((materialFlags & MATERIAL_FLAG_NORMAL_TBN) != 0)
+    {
+        result.normal = TBN[2];
+    }
+
+    return result;
+}
 
 int GetDiffuseTextureID(S_MATERIAL* material)
 {
--- shaders/default_pbr.frag
+++ shaders/default_pbr.frag
@@ -20,49 +20,15 @@ ForwardPassOutput main(PSInput input)
 {
     S_INSTANCE* InstancePtr = GetInstance();
     S_MATERIAL* MaterialPtr = GetMaterial(InstancePtr);
-    uint materialFlags = GetMaterialFlags(MaterialPtr);
-
-    float3 diffuse    = SampleGlobalTextureAnisotropic(GetDiffuseTextureID(MaterialPtr), input.uv).rgb;
-    float3 normal     = SampleGlobalTextureAnisotropic(GetNormalTextureID(MaterialPtr), input.uv).rgb;
-
-    float metalness   = 0.0;
-    float roughness   = 0.0;
-
-    if ((materialFlags & MATERIAL_FLAG_MATERIAL_TEXTURES_DEFAULT) != 0)
-    {
-        metalness = SampleGlobalTextureAnisotropic(GetMetalnessTextureID(MaterialPtr), input.uv).r;
-        roughness = SampleGlobalTextureAnisotropic(GetRoughnessTextureID(MaterialPtr), input.uv).r;
-    }
-    if ((materialFlags & MATERIAL_FLAG_MATERIAL_TEXTURES_GLTF_SPHERES) != 0)
-    {
-        float2 metalness_roughness = SampleGlobalTextureAnisotropic(GetMetalnessTextureID(MaterialPtr), input.uv).bg;
-        metalness = metalness_roughness.x;
-        roughness = metalness_roughness.y;
-    }
-
-    float3 n = float3(0,0,1);
-    if ((materialFlags & MATERIAL_FLAG_NORMAL_TEXTURE) != 0)
-    {
-        n = normalize(mul(input.TBN, (normal * 2.0 - 1.0)));
-    }
-
-    if ((materialFlags & MATERIAL_FLAG_NORMAL_TEXTURE_INVERT_Y) != 0)
-    {
-        normal = float3(normal.r, 1 - normal.g, normal.b);
-        n = normalize(mul(input.TBN, (normal * 2.0 - 1.0)));
-    }
-
-    if ((materialFlags & MATERIAL_FLAG_NORMAL_TBN) != 0)
-    {
-        n = input.TBN[2];
-    }
+    
+    S_MATERIAL_PROPERTIES MaterialProperties = ReadMaterialProeprties(MaterialPtr, input.uv, input.TBN);
 
     S_SURFACE surface;
     surface.wpos = input.wpos;
-    surface.normal = n;
-    surface.diffuseSpectrum = diffuse;
-    surface.roughness = roughness;
-    surface.metalness = metalness;
+    surface.normal = MaterialProperties.normal;
+    surface.diffuseSpectrum = MaterialProperties.diffuse;
+    surface.roughness = MaterialProperties.roughness;
+    surface.metalness = MaterialProperties.metalness;
     surface.prevWpos = float4(input.prev_wpos, 1.0);
 
     S_LIGHTING_RESULT lighting = CalculateLighting(surface);
